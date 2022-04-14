c43607961e0524421ccef5d0e8af828b832759ca
nadav1996700 <nadav1996700@gmail.com>
2022-04-14 19:21:07 +0300

    finish design of personal details fragment

---
+++ app/src/main/java/src/Utils/DatePickerFragment.java
@@ -0,0 +1,39 @@
+package src.Utils;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    private EditText editText;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void setEditText(EditText editText) {
+        this.editText = editText;
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Do something with the date chosen by the user
+        String date = day + "/" + (month + 1) + "/" + year;
+        editText.setText(date);
+    }
+}
--- app/src/main/java/src/fragments/FragmentPersonalInfo.java
+++ app/src/main/java/src/fragments/FragmentPersonalInfo.java
@@ -1,16 +1,31 @@
 package src.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 
 import com.example.src.R;
+import com.google.android.material.button.MaterialButton;
+
+import src.Utils.DatePickerFragment;
 
 public class FragmentPersonalInfo extends Fragment {
+    protected View view;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText birthday;
+    private EditText id;
+    private EditText email;
+    private EditText phone;
+    private ImageButton pickDate;
+    private MaterialButton btnContinue;
 
     public FragmentPersonalInfo() {
         // Required empty public constructor
@@ -40,6 +55,32 @@ public class FragmentPersonalInfo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_info, container, false);
+        if (view == null)
+            view = inflater.inflate(R.layout.fragment_personal_info, container, false);
+        // bind variables
+        bindVariables();
+        pickDate.setOnClickListener(v -> showDatePickerDialog(v));
+        return view;
+    }
+
+    public void showDatePickerDialog(View v) {
+        try {
+            DatePickerFragment newFragment = new DatePickerFragment();
+            newFragment.setEditText(birthday);
+            newFragment.show(getActivity().getSupportFragmentManager(), "DatePicker");
+        } catch (NullPointerException ex) {
+            Log.d("ERROR", "ERROR ON FRAGMENT MANAGER");
+        }
+    }
+
+    private void bindVariables() {
+        firstName = view.findViewById(R.id.personalInfo_EDT_firstName);
+        lastName = view.findViewById(R.id.personalInfo_EDT_lastName);
+        birthday = view.findViewById(R.id.personalInfo_EDT_pickDate);
+        id = view.findViewById(R.id.personalInfo_EDT_id);
+        email = view.findViewById(R.id.personalInfo_EDT_email);
+        phone = view.findViewById(R.id.personalInfo_EDT_phone);
+        btnContinue = view.findViewById(R.id.personalInfo_BTN_continue);
+        pickDate = view.findViewById(R.id.personalInfo_BTN_pickDate);
     }
 }
